83c0acab0ee627289ac4690be97076b2c7124a78
Junaid Syed <sjunaid626@gmail.com>
2023-07-28 20:25:39 +0530

    create: project data

---
+++ src/data/projectsData.js
@@ -0,0 +1,9 @@
+const projectsData = [
+    {
+        id: 1,
+        name: 'Project 1',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+    },
+];
+
+export default projectsData;
