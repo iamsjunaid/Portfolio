c9e89cd21c89a4c7e49cc4c25227cf1587d759f4
Junaid Syed <sjunaid626@gmail.com>
2023-08-03 16:40:47 +0530

    add contact and testimonials

---
+++ src/data/testimonials.js
@@ -0,0 +1,21 @@
+export const testimonials = [
+  {
+    quote: "I'm amazed by the quality of their service. The team was professional and delivered beyond my expectations.",
+    image: "https://example.com/images/person1.jpg",
+    name: "John Doe",
+    company: "ABC Corporation"
+  },
+  {
+    quote: "Their product helped me streamline my workflow and increase productivity. I can't imagine working without it.",
+    image: "https://example.com/images/person2.jpg",
+    name: "Jane Smith",
+    company: "XYZ Solutions"
+  },
+  {
+    quote: "The support I received from their team was outstanding. They went above and beyond to resolve my issues.",
+    image: "https://example.com/images/person3.jpg",
+    name: "Michael Johnson",
+    company: "123 Tech"
+  },
+  // Add more testimonials...
+];
