9acaab33a6a67d80aff4c31d73fc2078319353e4
Junaid Syed <sjunaid626@gmail.com>
2023-08-03 19:00:00 +0530

    add id key in testimonials data

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -1,17 +1,20 @@
 export const testimonials = [
   {
+    id: 1,
     quote: "I'm amazed by the quality of their service. The team was professional and delivered beyond my expectations.",
     image: "https://example.com/images/person1.jpg",
     name: "John Doe",
     company: "ABC Corporation"
   },
   {
+    id: 2,
     quote: "Their product helped me streamline my workflow and increase productivity. I can't imagine working without it.",
     image: "https://example.com/images/person2.jpg",
     name: "Jane Smith",
     company: "XYZ Solutions"
   },
   {
+    id: 3,
     quote: "The support I received from their team was outstanding. They went above and beyond to resolve my issues.",
     image: "https://example.com/images/person3.jpg",
     name: "Michael Johnson",
