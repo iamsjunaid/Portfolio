b253d3c8eccfb2a3f25330ab9713f45968d3e1c2
Junaid Syed <sjunaid626@gmail.com>
2023-08-03 21:36:16 +0530

    create footer

---
+++ src/pages/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <section id="Footer">
+      <div className="py-2 w-full bg-gray-800 text-white title-font font-medium text-center"><p>Footer</p></div>
+    </section>
+  )
+}
+
+export default Footer
